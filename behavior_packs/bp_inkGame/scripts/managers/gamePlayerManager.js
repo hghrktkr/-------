9fa373d63c786f12128acd504f75ba51f1fcd6a1
hghrktkr <147295919+hghrktkr@users.noreply.github.com>
2025-10-24 13:14:18 +0900

    GamePlayerManagerクラス作成

---
+++ behavior_packs/bp_inkGame/scripts/managers/gamePlayerManager.js
@@ -0,0 +1,36 @@
+class GamePlayerManager {
+
+    constructor() {
+        this.gamePlayers = new Map();
+    }
+
+    
+
+    addGamePlayer(player) {
+        if(!this.gamePlayers.has(player.id)) {
+            const gamePlayer = new GamePlayer(player);
+            this.gamePlayers.set(player.id, gamePlayer);
+            console.log(`Added GamePlayer: ${player.name} (ID: ${player.id})`);
+        }
+    }
+
+    checkDeadPlayer(deadEntityId) {
+        const gamePlayer = this.gamePlayers.get(deadEntityId);
+        if(gamePlayer) {
+            gamePlayer.onDeath();
+            console.log(`Player ${gamePlayer.name} has died.`);
+        }
+    }
+
+    checkPlayerGameMode() {
+        this.gamePlayers.forEach((gamePlayer) => {
+            if(gamePlayer.player.gameMode !== GameMode.Adventure) {
+                gamePlayer.player.setGameMode(GameMode.Adventure);
+                console.log(`Reset game mode for player ${gamePlayer.name} to Adventure.`);
+            }
+        });
+    }
+
+}
+
+export const gamePlayerManager = new GamePlayerManager();
