f4eb6f94fd676236c0842bedb12370d9a879751c
hghrktkr <147295919+hghrktkr@users.noreply.github.com>
2025-10-27 11:05:40 +0900

    チーム振り分け処理を追加

--- behavior_packs/bp_inkGame/scripts/managers/gamePlayerManager.js
+++ behavior_packs/bp_inkGame/scripts/managers/gamePlayerManager.js
@@ -1,14 +1,15 @@
 import { GamePlayer } from "../classes/gamePlayer";
-import { GameMode } from "@minecraft/server";
+import { GameMode, ItemStack } from "@minecraft/server";
+import { teamConfig } from "../configs/playerConfig";
 
 class GamePlayerManager {
 
     constructor() {
         this.gamePlayers = new Map();
+        this.BlueTeamPlayers = new Set();
+        this.YellowTeamPlayers = new Set();
     }
-
-    
-
+ 
     addGamePlayer(player) {
         if(!this.gamePlayers.has(player.id)) {
             const gamePlayer = new GamePlayer(player);
@@ -33,6 +34,43 @@ class GamePlayerManager {
             }
         });
     }
+
+    addBlueTeamPlayer(gamePlayer) {
+        if(this.BlueTeamPlayers.has(gamePlayer)) return;
+        if(this.YellowTeamPlayers.has(gamePlayer)) {
+            this.YellowTeamPlayers.delete(gamePlayer);
+            console.log(`Player ${gamePlayer.name} removed from Yellow Team.`);
+        }
+
+        this.BlueTeamPlayers.add(gamePlayer);
+        console.log(`Player ${gamePlayer.name} added to Blue Team.`);
+        gamePlayer.setTeam(teamConfig.teamBlue.teamColor, teamConfig.teamBlue.teamColorEntityType, teamConfig.teamBlue.teamColorBlockType);
+
+        // edu:bib_blueを装備、解除不可にする
+        const blueBib = new ItemStack("edu:bib_blue", 1)
+                            .keepOnDeath = true
+                            .lockMode = "slot";
+        gamePlayer.player.getComponent("minecraft:equippable").setEquipment("chest", blueBib);
+    }
+
+    addYellowTeamPlayer(gamePlayer) {
+        if(this.YellowTeamPlayers.has(gamePlayer)) return;
+        if(this.BlueTeamPlayers.has(gamePlayer)) {
+            this.BlueTeamPlayers.delete(gamePlayer);
+            console.log(`Player ${gamePlayer.name} removed from Blue Team.`);
+        }
+
+        this.YellowTeamPlayers.add(gamePlayer);
+        console.log(`Player ${gamePlayer.name} added to Yellow Team.`);
+        gamePlayer.setTeam(teamConfig.teamYellow.teamColor, teamConfig.teamYellow.teamColorEntityType, teamConfig.teamYellow.teamColorBlockType);
+        
+        // edu:bib_yellowを装備、解除不可にする
+        const yellowBib = new ItemStack("edu:bib_yellow", 1)
+                            .keepOnDeath = true
+                            .lockMode = "slot";
+        gamePlayer.player.getComponent("minecraft:equippable").setEquipment("chest", yellowBib);
+    }
+
 
 }
 
