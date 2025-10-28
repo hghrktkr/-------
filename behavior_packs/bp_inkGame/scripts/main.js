604eaf0d5e97bc6ed26bc0a980c37f41375c5726
hghrktkr <147295919+hghrktkr@users.noreply.github.com>
2025-10-28 19:29:42 +0900

    イベントトリガー整理

--- behavior_packs/bp_inkGame/scripts/main.js
+++ behavior_packs/bp_inkGame/scripts/main.js
@@ -1,14 +1,17 @@
 // Minecraftで実行
 // main.js
-import { system, world } from "@minecraft/server";
+import { ItemLockMode, ItemStack, system, world } from "@minecraft/server";
 import { gamePlayerManager } from "./managers/gamePlayerManager.js";
 import { inkGun } from "./classes/inkGun.js";
-import { teamConfig } from "./configs/playerConfig.js";
+import { flagManager } from "./managers/flagManager.js";
+import { gameManager } from "./managers/gameManager.js";
 
 // ワールド読み込み時
 world.afterEvents.worldLoad.subscribe(ev => {
     console.log("World loaded.");
 });
+
+
 
 world.afterEvents.playerSpawn.subscribe(ev => {
     const { player } = ev;
@@ -17,7 +20,7 @@ world.afterEvents.playerSpawn.subscribe(ev => {
 
     const gamePlayer = gamePlayerManager.gamePlayers.get(player.id);
     if(gamePlayer) {
-        gamePlayer.setTeam(teamConfig.teamBlue.teamColor, teamConfig.teamBlue.teamColorEntityType, teamConfig.teamBlue.teamColorBlockType);
+        gamePlayerManager.addBlueTeamPlayer(gamePlayer);
     }
 });
 
@@ -25,6 +28,7 @@ world.afterEvents.playerSpawn.subscribe(ev => {
 // エンティティが死んだ時
 world.afterEvents.entityDie.subscribe(ev => {
     console.log(`Entity died: ${ev.deadEntity.id}`);
+    if(gameManager.gameState !== "PLAYING") return;
     // 死亡したエンティティがプレイヤーかチェック
     const deadEntityId = ev.deadEntity.id;
     gamePlayerManager.checkDeadPlayer(deadEntityId);
@@ -35,11 +39,24 @@ world.afterEvents.entityDie.subscribe(ev => {
 // 毎tickチェック
 system.runInterval(() => {
     // ゲームモード・ゲームルールの監視
+    if(gameManager.gameState !== "PLAYING")　return;
     gamePlayerManager.checkPlayerGameMode();
     for(const gamePlayer of gamePlayerManager.gamePlayers.values()) {
         gamePlayer.updatePerTick();
     }
 }, 1);
+
+
+
+world.afterEvents.playerInventoryItemChange.subscribe((ev) => {
+    const { player, itemStack } = ev;
+
+    const gamePlayer = gamePlayerManager.gamePlayers.get(player.id);
+    if(!gamePlayer || gameManager.gameState !== "PLAYING") return;
+    if(itemStack.typeId === "edu:flag") {
+        flagManager.onGetFlag(gamePlayer);
+    }
+});
 
 
 
