189537d93fbd9417f1e52ff1914f75a8a043024a
hghrktkr <147295919+hghrktkr@users.noreply.github.com>
2025-10-28 10:40:10 +0900

    bib、InkGunの装備メソッドを追加

--- behavior_packs/bp_inkGame/scripts/managers/gamePlayerManager.js
+++ behavior_packs/bp_inkGame/scripts/managers/gamePlayerManager.js
@@ -1,5 +1,5 @@
 import { GamePlayer } from "../classes/gamePlayer";
-import { GameMode, ItemStack } from "@minecraft/server";
+import { EquipmentSlot, GameMode, ItemLockMode, ItemStack } from "@minecraft/server";
 import { teamConfig } from "../configs/playerConfig";
 
 class GamePlayerManager {
@@ -46,11 +46,8 @@ class GamePlayerManager {
         console.log(`Player ${gamePlayer.name} added to Blue Team.`);
         gamePlayer.setTeam(teamConfig.teamBlue.teamColor, teamConfig.teamBlue.teamColorEntityType, teamConfig.teamBlue.teamColorBlockType);
 
-        // edu:bib_blueを装備、解除不可にする
-        const blueBib = new ItemStack("edu:bib_blue", 1)
-                            .keepOnDeath = true
-                            .lockMode = "slot";
-        gamePlayer.player.getComponent("minecraft:equippable").setEquipment("chest", blueBib);
+        this.equipPlayerBib(gamePlayer, "edu:bib_blue");
+        this.givePlayerInkGun(gamePlayer);
     }
 
     addYellowTeamPlayer(gamePlayer) {
@@ -63,12 +60,26 @@ class GamePlayerManager {
         this.YellowTeamPlayers.add(gamePlayer);
         console.log(`Player ${gamePlayer.name} added to Yellow Team.`);
         gamePlayer.setTeam(teamConfig.teamYellow.teamColor, teamConfig.teamYellow.teamColorEntityType, teamConfig.teamYellow.teamColorBlockType);
-        
-        // edu:bib_yellowを装備、解除不可にする
-        const yellowBib = new ItemStack("edu:bib_yellow", 1)
-                            .keepOnDeath = true
-                            .lockMode = "slot";
-        gamePlayer.player.getComponent("minecraft:equippable").setEquipment("chest", yellowBib);
+
+        this.equipPlayerBib(gamePlayer, "edu:bib_yellow");
+        this.givePlayerInkGun(gamePlayer);
+    }
+
+    equipPlayerBib(gamePlayer, bibItemId) {
+        const bib = new ItemStack(bibItemId, 1);
+        bib.keepOnDeath = true;
+        bib.lockMode = ItemLockMode.slot;
+        gamePlayer.player.getComponent("minecraft:equippable").setEquipment(EquipmentSlot.Chest, bib);
+    }
+
+    givePlayerInkGun(gamePlayer) {
+        const inkGun = new ItemStack("edu:ink_gun", 1);
+        inkGun.name = "インクガン";
+        inkGun.keepOnDeath = true;
+        inkGun.lockMode = ItemLockMode.inventory;
+        inkGun.setCanDestroy(["edu:flag"]);
+        gamePlayer.player.getComponent("minecraft:inventory").container.addItem(inkGun);
+        console.log(`Gave Ink Gun to player ${gamePlayer.name}.`);
     }
 
 
