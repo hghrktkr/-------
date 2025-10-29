42bc63666989ac1845cecc8609757a6b569355ae
hghrktkr <147295919+hghrktkr@users.noreply.github.com>
2025-10-30 00:16:55 +0900

    スポーン位置の変更

--- behavior_packs/bp_inkGame/scripts/configs/playerConfig.js
+++ behavior_packs/bp_inkGame/scripts/configs/playerConfig.js
@@ -2,7 +2,7 @@
 
 /** 初期スポーン位置、ディメンション */
 export const playerSpawnPosition = {
-    spawnPoint: { x: 0, y: 66, z: 0 },
+    spawnPoint: { x: 0, y: 0, z: 40 },
     spawnDimension: "overworld"
 };
 
@@ -12,19 +12,19 @@ export const teamConfig = {
         teamColor: "blue",
         teamColorEntityType: "edu:falling_ink_blue",
         teamColorBlockType: "edu:ink_blue",
-        teamAreaStart: {x: -30, y: 0, z: -30},
-        teamAreaEnd: {x: -30, y: 0, z: 0},
-        teamSpawnPos: {x: -30, y: 0, z: -15}
+        teamAreaStart: {x: -48, y: 8, z: -2},
+        teamAreaEnd: {x: -44, y: 8, z: 2},
+        teamSpawnPos: {x: -46, y: 8, z: 0}
     },
     teamYellow: {
         teamColor: "yellow",
         teamColorEntityType: "edu:falling_ink_yellow",
         teamColorBlockType: "edu:ink_yellow",
-        teamAreaStart: {x: 30, y: 0, z: -30},
-        teamAreaEnd: {x: 30, y: 0, z: 0},
-        teamSpawnPos: {x: 30, y: 0, z: -15}
+        teamAreaStart: {x: 44, y: 8, z: -2},
+        teamAreaEnd: {x: 48, y: 8, z: 2},
+        teamSpawnPos: {x: 46, y: 8, z: 0}
     },
     spectator: {
-        spawnPos: {x: 0, y: 0, z: 0 }
+        spawnPos: {x: 0, y: 10, z: 0 }
     }
 }
