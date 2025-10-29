9b6e4a217a45548c2ee53fd66ccdbee626f215c6
hghrktkr <147295919+hghrktkr@users.noreply.github.com>
2025-10-30 01:55:00 +0900

    旗を落とす条件にisOnGround追加

--- behavior_packs/bp_inkGame/scripts/classes/gamePlayer.js
+++ behavior_packs/bp_inkGame/scripts/classes/gamePlayer.js
@@ -130,7 +130,7 @@ export class GamePlayer {
     }
 
     checkOutOfInk() {
-        if(!this.isInInk && this.hasFlag && !this.player.isJumping) {
+        if(!this.isInInk && this.hasFlag && this.player.isOnGround) {
             flagManager.onDamagedDropFlag(this);
         }
     }
